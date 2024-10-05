d406aab817c13a2d080bb2c19c15e7ccb7c53534
pogrebenny21 <pogrebennyjaleksej235@gmail.com>
2024-10-05 18:21:49 +1000

    новый файл

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ strength = 5
 agility = 5
 intellect = 5
 lvl = 1
+damage = 5
+defence = 5
 
 print("Здоровье:", hp, "Стас Давыдов:", mana)
 print(f"Сила: {strength}\nЛовкость: {agility}\nИнтеллект: {intellect}")
@@ -23,18 +25,24 @@ _class = input("Выберите класс: 1. язь, 2. адвокат, 3. п
 if _class == "язь" or _class== "1":
     strength = strength * 2
     agility *= 1.6 
+    damage = 5
+    defence = 5
     _class = "язь"
 elif _class == "адвокат" or _class== "2":
     intellect *= 2
     agility *= 1.3
+    damage = 5
+    defence = 5
     _class == "адвокат"
 elif _class == "паукан" or _class== "3":
     agility *= 2
     strength *= 1.5
     intellect *= 1.3
+    damage = 5
+    defence = 5
     _class == "паукан"
 else: 
-    print("ФАТАЛЬНАЯ ОШИБКА, ты Говно!.\nВсе характеристики понижены на 3")
+    print("ФАТАЛЬНАЯ ОШИБКА, ты Говнcnfcо!.\nВсе характеристики понижены на 3")
     strength -= 3
     agility -= 3
     intellect -= 3
@@ -49,7 +57,6 @@ for i in range(5,100,2):
 n = 0
 
 
-
 isGame = True
 while isGame:
     print("Игра Началась")
@@ -57,11 +64,38 @@ while isGame:
     rand = random.randint(1,6)
     print("выпало", rand)
     print("Здоровье на исходе:" , hp)
+
+    rand = random.randint(1,6)
+
+    print("Выпало" , rand)
+
+    if rand == 2:
+        hpEnemy = 100
+        damageEnemy = 75
+        while True:
+            print("Перед Вами Вылез Адвокат Что Будем Делать?")
+            Choice = input("1. аттоковать 2. Защититься 3. увернутся")
+            if Choice == "1":
+                hpEnemy -= damage
+                print(f"АДВОКАТ ПОЛУЧИЛ ОПЛЕУХУ!! У НЕГО ОСТАЛОСЬ {hpEnemy}")
+            elif Choice =="2":
+                if damageEnemy > defence:
+                    hp = hp - (damageEnemy-defence) 
+                    print(f"Адвокат наносит {damageEnemy-defence} Урона")
+                else:
+                    print("Невироятная Защита! Урон Полностью Блокирован!")
+            
+            elif Choice == "3":
+                print("Уворот")
+            if (hpEnemy<=0):
+                print("ПОБЕДА!!")
+                break
+                
     if hp <= 0:
         print("Остаток Здоровья:" , hp)
         print("WASTED!")
         isGame = False
-        break
+        break       
     time.sleep(0.5)
 
 
